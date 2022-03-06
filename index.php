d0171120ed617fe89951b61257f5972972423fd7
Jia Qi Feng <jiaqifen@buffalo.edu>
2022-03-06 00:23:00 -0500

    #26 Moved index.php file outside of public_html folder

--- index.php
+++ index.php
@@ -31,3 +31,19 @@
     }
 ?>
 
+<html lang="en">
+    <head>
+        <title>Bug Busters</title>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="/app/assets/css/app.css" type="text/css">
+    </head>
+    <body>
+
+        <div id="app"></div>
+
+        <script type="text/javascript" src="/app/assets/bundle/main.bundle.js" ></script>
+
+    </body>
+</html>
