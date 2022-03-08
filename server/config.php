dc822dded732306144d2ca83bba543d0eb7dca6c
Jia Qi Feng <jiaqifen@buffalo.edu>
2022-03-07 22:58:28 -0500

    #32 tagging this task now because it was not showing up earlier during branch 26 merge.

--- server/config.php
+++ server/config.php
@@ -4,6 +4,6 @@
 
   $db_config["dbServername"] = "oceanus.cse.buffalo.edu";
   $db_config["dbUsername"] = "jiaqifen";
-  $db_config["dbName"] = "cse442_2022_spring_team_h_db";
   $db_config["dbPassword"] = "50235120";
+  $db_config["dbName"] = "cse442_2022_spring_team_h_db";
 ?>
