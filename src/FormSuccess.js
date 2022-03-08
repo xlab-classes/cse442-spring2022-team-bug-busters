cb660e48580f0378c21525d18320ac070c700313
Rachaelj99 <rachaeljeon99@gmail.com>
2022-03-07 19:28:14 -0500

    #28 Created login page
    I just created a simple sign up page

---
+++ src/FormSuccess.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Form.css";
+
+const FormSuccess = () => {
+  return (
+    <div className="form">
+      <h1 className="form-success-request">
+        <success>We have received your request!</success>
+      </h1>
+    </div>
+  );
+};
+
+export default FormSuccess;
