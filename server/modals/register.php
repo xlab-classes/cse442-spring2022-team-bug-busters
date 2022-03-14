22f9c6f7e5e7ef1e5b733a21afabc4fb5ede8940
Rachaelj99 <rachaeljeon99@gmail.com>
2022-03-14 11:01:25 -0400

    inserted one dummy data

--- server/modals/register.php
+++ server/modals/register.php
@@ -1,3 +1,34 @@
 <?php
-echo "Register!";
+
+    include("dbConnect.php");
+
+    $username = "rsjeon";
+    $password = "password1";
+
+    if ($conn->connect_error){
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "SELECT * FROM users(username, hashed_pw)";
+
+    $result = mysql_query($sql);
+    if ($result) {
+        if (mysql_num_rows($result) > 0) {
+            echo 'User already exists!';
+        } else {
+            dbConnect.addUser($username, $password);
+            echo 'Added a user';
+        }
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    $conn->close();
+
 ?>
