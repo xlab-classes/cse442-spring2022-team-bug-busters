17e4018effa99295b33701ce1db5bee344425741
Richard977 <60375441+Richard977@users.noreply.github.com>
2022-04-07 22:47:19 -0400

    #48 finish front end for waiting room page

--- src/Component/WaitingRoom.jsx
+++ src/Component/WaitingRoom.jsx
@@ -13,17 +13,42 @@ export default class WaitingRoom extends React.Component {
           <NavBar></NavBar>
         </div>
         <div className='waitingRoomContents'>
-          <div>
+          <div className='gameName'>
             Play Bug Busters
           </div>
-          <div>
+          <div className='player'>
             Players
           </div>
-          <div className='players'>
-            user1
-            user2
-            user3
-            user4
+          <div className='user'>
+            <div className='userrow'>
+              user1
+            </div>
+            <div className='userrow'>
+              user2
+            </div>
+            <div className='userrow'>
+              user3
+            </div>
+            <div className='userrow'>
+              user4
+            </div>
+            <div className='userrow'>
+              user5
+            </div>
+            <div className='userrow'>
+              user5
+            </div>
+          </div>
+          <div className='start'>
+            Start Game
+          </div>
+
+          <div className='roomCode'>
+            <div>
+              {"Room Code: " + "room code"}
+            </div>
+
+            
           </div>
         </div>
         
