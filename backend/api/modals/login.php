0f8bbf9d85c705949dac3aa8e3fdbf982cd6f549
Jia Qi Feng <jiaqifen@buffalo.edu>
2022-04-11 00:14:55 -0400

    #52 attempting to connect to the API under the server

--- backend/api/modals/login.php
+++ backend/api/modals/login.php
@@ -5,11 +5,12 @@
     header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
     header("Allow: GET, POST, OPTIONS, PUT, DELETE");
     $_POST = json_decode(file_get_contents("php://input"));
-
+    $method = $_SERVER['REQUEST_METHOD'];
+    
     if (empty($_POST['username']) && empty($_POST['password'])){
         die();
     }
-    if($_POST){
+    if($method == 'POST'){
         $db = new DBConnection($db_config);
         $db = $db -> getConnection();
 
@@ -26,7 +27,8 @@
         echo json_encode($data);
     }
     
-
+    $db->close();
+    
     function generateAuth(){
         $token = bin2hex(openssl_random_pseudo_bytes(60));
         return $token;
