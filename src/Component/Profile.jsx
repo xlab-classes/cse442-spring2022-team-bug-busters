5e2cb83d354d829df5b62a96362907fdaa3fbe43
Rachaelj99 <rachaeljeon99@gmail.com>
2022-04-12 18:03:06 -0400

    added connection

--- src/Component/Profile.jsx
+++ src/Component/Profile.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
 import "../Profile.css"
 import NavBar from "./NavBar"
+import axios from "axios";
 export default function Profile() {
+
+  const [state, setState] = React.useState([]);
+
+  // Only runs when the component gets moounted
+  useEffect(() => {
+    getPoints();
+  }, []);
+
+  function getPoints(){
+    axios.get('http://localhost:8080/backend/modals/dbqueries.php').then(function(response) {
+      console.log(response.data);
+      setState(response.data);
+    });
+  }
+
   return (
+
     <div>
       {/* <div id='navbar'>
         <div id="navbarLeftside">
@@ -22,7 +39,8 @@ export default function Profile() {
         <div id = 'profileInformation'>
           <p>Player Name</p>
           <p>Rank #4104</p>
-          <p>Wins: 103 | Losses: 34</p>
+          <p name='wins' value={state.wins} onChange={handleChange}> Wins: {state.wins} | </p>
+          <p name='losses' value={state.losses} onChange={handleChange}> Losses: {state.losses}</p>
         </div>
 
       </div>
