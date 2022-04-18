951e0c2bc8d1220dde47e5d5b9e89567a83d3809
Rachaelj99 <rachaeljeon99@gmail.com>
2022-04-17 23:12:23 -0400

    #37 call request()

--- api/modals/register.php
+++ api/modals/register.php
@@ -1,47 +1,14 @@
 <?php
-    include("./helper.php");
-    include_once("./config.php");
-    include_once("./dbqueries.php");
-    include_once("./dbConnect.php");
-    header('Access-Control-Allow-Origin: *');
-    header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, PATCH, DELETE");
-    header("Allow: GET, POST, OPTIONS, PUT, DELETE");
-    header("Access-Control-Allow-Headers: *");
-    header("Access-Control-Allow-Headers: 'X-Requested-With,content-type'");
-    header('Access-Control-Max-Age: 1728000');
-    header("Content-Length: 0");
-    header("Content-Type: text/plain"); 
-    $_POST = json_decode(file_get_contents("php://input"));
-    $method = $_SERVER['REQUEST_METHOD'];
-    echo "received data";
-    print_r($_POST);
-    
-    if ($method == 'POST'){
-        if (empty($_POST['username']) || empty($_POST['password'])){
-            die();
-        }
+    include_once("registerController.php");
 
-        $db = new DBConnection($db_config);
-        $db = $db -> getConnection();
-        $userDataBase = new usersHelper($db);
-        $token = generateAuth();
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json; charset=UTF-8");
+    header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
+    header("Access-Control-Max-Age: 3600");
+    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-        $data = [
-            "firstname" => $_POST['firstname'],
-            "lastname" => $_POST['lastname'],
-            "username" => $_POST['username'],
-            "password" => $_POST['password'],
-            "msg" =>"success",
-            "token" => $token
-        ];
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-        $userDataBase->addUser($username, $password);
-        
-        echo "You are registered, ". $_POST['name']. "!";
-        echo json_encode($data);
-    }
-    
-    // $db->close();
-    $userDataBase->close();
+    $requestMethod = $_SERVER["REQUEST_METHOD"];
+  
+    $controller = new registerController($dbConnection, $requestMethod, $username);
+    $controller->processRequest();
 ?>
