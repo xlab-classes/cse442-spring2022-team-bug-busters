093d0b65f7eb326c784cdc07992a54c8a9cbbbf4
Rachaelj99 <rachaeljeon99@gmail.com>
2022-04-21 18:58:06 -0400

    #37 making a working backend register page

--- backend/api/modals/register.php
+++ backend/api/modals/register.php
@@ -1,3 +1,34 @@
 <?php
-echo "Register!";
+    require("../helper.php");
+    require("../config.php");
+    require("../dbConnect.php");
+    require("../dbqueries.php");
+
+    http_response_code(200);
+    header("Content-Type: application/json");
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+    $method = $_SERVER['REQUEST_METHOD'];
+    $_POST = json_decode(file_get_contents("php://input"), TRUE);
+
+    if($method == 'POST'){
+        $db = new DBConnection($db_config);
+        $db = $db -> getConnection();
+        $userdb = new usersHelper($db);
+        $username = $_POST['username'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+
+        $user = $userdb->addUser($username, $email, $password);
+        if ($user === "This user already exists!"){
+            http_response_code(401);
+        }
+        else {
+            $data = array();
+            $data['message'] = "You are now registered from the backend!";
+            echo json_encode($data);
+        }        
+        $db->close();
+    }
 ?>
