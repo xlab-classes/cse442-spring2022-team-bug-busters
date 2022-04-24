46a3391c3bb8559fe108db03d7455572e302ae54
Rachaelj99 <rachaeljeon99@gmail.com>
2022-04-24 17:39:53 -0400

    added testing url

--- src/Component/FormSignup.js
+++ src/Component/FormSignup.js
@@ -2,8 +2,9 @@ import React from "react";
 import { AppIndicator } from "react-bootstrap-icons";
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
-// const API = "https://www-student.cse.buffalo.edu/CSE442-542/2022-Spring/cse-442h/backend/api/modals/register.php"
-const API = "http://localhost:8080/modals/";
+ const API = "https://www-student.cse.buffalo.edu/CSE442-542/2022-Spring/cse-442h/backend/api/modals/register.php"
+// Testing URL
+//const API = "http://localhost:8080/modals/";
 
 export default function Form() {
   
