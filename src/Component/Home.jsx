c1f864b1b863babe5191ab6014bc40ef3de9ea3a
Rachaelj99 <rachaeljeon99@gmail.com>
2022-05-02 17:38:51 -0400

    #50 display points on the home page

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import "../Profile.css";
 import NavBar from "./NavBar";
 import pic0 from "../assets/profile_pictures/pic0.png";
+import Leaderboard from './Leaderboard';
 // Use the following line for deployment!
 //const API = "https://www-student.cse.buffalo.edu/CSE442-542/2022-Spring/cse-442h/backend/api/modals/"
 
@@ -51,6 +52,7 @@ export default class HomePage extends React.Component {
                 <div className="containerSettings">
                     <div id="title">
                         <h2>Leaderboard</h2>
+                        <Leaderboard></Leaderboard>
                     </div>
                 </div>
             </div>
