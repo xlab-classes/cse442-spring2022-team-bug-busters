919f3de79c260a865a21b090af08940def6ae789
Rachaelj99 <rachaeljeon99@gmail.com>
2022-05-03 22:45:54 -0400

    #37 add more styling

--- src/Component/FormSignup.jsx
+++ src/Component/FormSignup.jsx
@@ -76,36 +76,49 @@ export default class SignUp extends React.Component {
 
     render() {
         return(
-            <div className="register1">
-                <div className="container">
-                    <div className="card">
+            // <div class="card text-center"> --------!
+            //     <div class="card-header"> --------!
+            //         Featured --------!
+            //     </div> --------!
+            //     <div class="card-body"> --------!
+            //         <h5 class="card-title">Special title treatment</h5> --------!
+            //         <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            //         <a href="#" class="btn btn-primary">Go somewhere</a>
+            //     </div> --------!
+            // </div>
+            <div class="card text-center">
+                <div class="card-header">
+                    Registration
+                </div>
+                <div class="card-body">
+                    {/* <div className="card"> */}
                         <form onSubmit={this.handleSubmit}>
-                            <h1>Let's play, Bug Busters!</h1>
-                            <h2>Create your account by filling out the information below.</h2>
+                            <br></br>
+                            <h1 class="card-title">Let's play, Bug Busters!</h1>
+                            <h5 class="card-subtitle">Create your account by filling out the information below.</h5>
                             {this.state.errors.register_failed && <div className="alert alert-danger" role="alert">Sorry, that username exists!</div>}
                             {this.state.errors.register_success && <div className="alert alert-success" role="alert">
                                 You have successfully registered! <a href="login" className="alert-link">Login Here!</a></div>}
-                            <div className="register">
                             <div className="form-group">
                                 <div className="username">
-                                <label class="col-sm-2 col-form-label">
-                                    <h3>Username</h3>
-                                    <input
-                                    type="text"
-                                    className="col-sm-4 form-control"
-                                    name="username"
-                                    value={this.state.username}
-                                    placeholder="Enter your username"
-                                    onChange={this.usernameChangeHandler}
-                                    required
-                                    />
-                                </label>
+                                    <label class="col-sm-3 col-form-label">
+                                        <p>Username</p>
+                                        <input
+                                        type="text"
+                                        className="col-sm-4 form-control"
+                                        name="username"
+                                        value={this.state.username}
+                                        placeholder="Enter your username"
+                                        onChange={this.usernameChangeHandler}
+                                        required
+                                        />
+                                    </label>
                                 </div>
                             </div>
                             <div className="form-group">
                                 <div className="email">
                                     <label class="col-sm-4 col-form-label">
-                                        <h4>Email</h4>
+                                        <p>Email</p>
                                         <input
                                         type="text"
                                         className="col-sm-4 form-control"
@@ -121,7 +134,7 @@ export default class SignUp extends React.Component {
                             <div className="form-group">
                                 <div className="password">
                                 <label class="col-sm-3 col-form-label">
-                                    <h4>Password</h4>
+                                    <p>Password</p>
                                     <input
                                     type="password"
                                     className="col-sm-4 form-control"
@@ -134,15 +147,15 @@ export default class SignUp extends React.Component {
                                 </label>
                                 </div>
                             </div>
-                            </div>
+                            <br></br>
                             <button type="submit" className="btn btn-primary">Submit</button>
                             <br></br>
                             <span className="login text-muted mt-5 mb-0">
                             Already have an account? Login <a href="login">here</a>
                             </span>
                         </form>
+                    {/* </div> */}
                     </div>
-                </div>
             </div>
         )
     }
