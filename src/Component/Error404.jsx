09168f4adda550dcdb3ae94d9a11d940a1e6bfd8
Richard977 <60375441+Richard977@users.noreply.github.com>
2022-05-04 00:44:15 -0400

    #62 fixed css for settings

--- src/Component/Error404.jsx
+++ src/Component/Error404.jsx
@@ -1,14 +1,14 @@
-const ErrorPage = () => {
-
-    const PUBLIC_URL = "/CSE442-542/2022-Spring/cse-442h";
+import React, { Component } from 'react'
+const PUBLIC_URL = "/CSE442-542/2022-Spring/cse-442h";
+export default class Error404 extends Component {
+  render() {
     return (<>
-    <div className="errorInfo">
-    <h1>Error: 404 PAGE NOT FOUND</h1>
-    <h2>Oops, looks like you accidentally wound up somewhere you're not supposed to be!</h2>
-    <h4>Please, <a href={PUBLIC_URL + "/"}>click here</a> to retun to the home page.</h4>
-    </div>
-    </>)
-
+        <div className="errorInfo">
+        <h1>Error: 404 PAGE NOT FOUND</h1>
+        <h2>Oops, looks like you accidentally wound up somewhere you're not supposed to be!</h2>
+        <h4>Please, <a href={PUBLIC_URL + "/"}>click here</a> to retun to the home page.</h4>
+        </div>
+        </>)
+  }
 }
 
-export default ErrorPage;
